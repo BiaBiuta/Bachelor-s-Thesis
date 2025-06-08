cf1b9a3eda6e0a53f6340b25cc024cdabff8f5ec
RobertDragota <121293266+RobertDragota@users.noreply.github.com>
2025-06-08 20:15:31 +0300

    Add nurse creation on user signup (#25)

--- event-calendar/accounts/forms.py
+++ event-calendar/accounts/forms.py
@@ -3,6 +3,8 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.password_validation import validate_password
 
 from accounts.models import User
+from calendarapp.models.nurse import Nurse
+from calendarapp.models.global_object import GlobalObject
 
 
 class SignInForm(forms.Form):
@@ -13,6 +15,14 @@ class SignInForm(forms.Form):
 
 
 class SignUpForm(forms.ModelForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    unitate_medicala = forms.ModelChoiceField(
+        label="Department",
+        queryset=GlobalObject.objects.all(),
+        widget=forms.Select(attrs={"class": "form-control"})
+    )
     password1 = forms.CharField(
         label="Password",
         widget=forms.PasswordInput(attrs={"class": "form-control"}),
@@ -24,9 +34,46 @@ class SignUpForm(forms.ModelForm):
         validators=[validate_password],
     )
 
+    employee_id = forms.CharField(
+        label="Employee ID",
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    max_shifts = forms.CharField(
+        label="Max Shifts",
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    max_total_mins = forms.IntegerField(
+        label="Max Total Minutes",
+        widget=forms.NumberInput(attrs={"class": "form-control"})
+    )
+    min_total_mins = forms.IntegerField(
+        label="Min Total Minutes",
+        widget=forms.NumberInput(attrs={"class": "form-control"})
+    )
+    max_cons_shifts = forms.IntegerField(
+        label="Max Consecutive Shifts",
+        widget=forms.NumberInput(attrs={"class": "form-control"})
+    )
+    min_cons_shifts = forms.IntegerField(
+        label="Min Consecutive Shifts",
+        widget=forms.NumberInput(attrs={"class": "form-control"})
+    )
+    min_cons_days_off = forms.IntegerField(
+        label="Min Consecutive Days Off",
+        widget=forms.NumberInput(attrs={"class": "form-control"})
+    )
+    max_weekends = forms.IntegerField(
+        label="Max Weekends",
+        widget=forms.NumberInput(attrs={"class": "form-control"})
+    )
+    days_off = forms.FloatField(
+        label="Days Off",
+        widget=forms.NumberInput(attrs={"class": "form-control"})
+    )
+
     class Meta:
         model = User
-        fields = ["email"]
+        fields = ["email", "username", "unitate_medicala"]
         widgets = {"email": forms.EmailInput(attrs={"class": "form-control"})}
 
     def clean_password2(self):
@@ -38,7 +85,23 @@ class SignUpForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
+        user.username = self.cleaned_data["username"]
+        unit = self.cleaned_data["unitate_medicala"]
+        # unitate_medicala este ModelChoiceField -> salvăm ID-ul
+        user.unitate_medicala = unit.id if hasattr(unit, "id") else unit
         user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
+            Nurse.objects.create(
+                EmployeeID=self.cleaned_data["employee_id"],
+                MaxShifts=self.cleaned_data["max_shifts"],
+                MaxTotalMins=self.cleaned_data["max_total_mins"],
+                MinTotalMins=self.cleaned_data["min_total_mins"],
+                MaxConsShifts=self.cleaned_data["max_cons_shifts"],
+                MinConsShifts=self.cleaned_data["min_cons_shifts"],
+                MinConsDaysOff=self.cleaned_data["min_cons_days_off"],
+                MaxWeekends=self.cleaned_data["max_weekends"],
+                DaysOff=self.cleaned_data["days_off"],
+                GlobalObject=unit,
+            )
         return user
