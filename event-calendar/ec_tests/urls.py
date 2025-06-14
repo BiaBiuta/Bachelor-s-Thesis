65fad52e686a63c9c34eacf66c040f5d7d13b6cd
RobertDragota <121293266+RobertDragota@users.noreply.github.com>
2025-06-14 19:57:36 +0300

    tests: add choose_instance url (#58)

--- event-calendar/ec_tests/urls.py
+++ event-calendar/ec_tests/urls.py
@@ -8,5 +8,6 @@ app_name = 'calendarapp'
 urlpatterns = [
     path('calendar/', include(([
         path('event/<int:pk>/', dummy, name='event-detail'),
+        path('instance/choose/', dummy, name='choose_instance'),
     ], 'calendarapp'), namespace='calendarapp')),
 ]
